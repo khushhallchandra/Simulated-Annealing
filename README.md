8e8122bbeb42c1f0ba6d324839b563acfc9469d0
Khushhall <kcm00000@gmail.com>
2016-10-22 22:42:36 +0530

    Simple model

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# VLSI-Project
-Simulated Annealing to minimize the wirelength
+# EE-677 VLSI CAD Course Project
+####  VLSI placement usning Simulated Annealing minimizing the wirelength
+#### PseudoCode:
+```python
+function annealing():
+    Temperature = INFINITE
+    # generate random configuration
+	grid = initialize()
+	minCost = cost(grid)
+	while(Temperature > threshold):
+    	tempGrid = findNeighbourState(grid)
+        tempCost = cost(tempGrid)
+		delta = tempCost - minCost
+		if (delta<0):
+			grid = tempGrid
+			minCost = tempCost
+		else:
+			probabilty = exp(-delta / Temperature)
+			if(random(0,1)<probabilty):
+			    grid = tempGrid
+				minCost = tempCost
+		Temperature = updateTemp(Temperature)
+	return grid
+```	
+
+```python
+function findNeighbourState(grid):
+	# We will follow these two protocols, each 50% of time
+	# 1. swap positions of any two elements
+	# 2. Move any element to an empty position in the grid
+	tempGrid = copy(grid)
+	if(random(0,1)<0.5):
+		element1 = select any element
+		element2 = select another element
+        swap element1 and element2 in tempGrid
+	else:
+		element1 = select any element
+		# find an empty position in the tempGrid
+		Move element1 to the empty position	
+	return tempGrid
+```	
+
+#### Group Members:
+##### Khushhall Chandra Mahajan
+##### Charvi Rastogi
--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -3,70 +3,126 @@ import math
 import numpy as np
 
 INF = 10000
-alpha = 0.9
-k = 10
-# let's say we are given a set of points in one dimension and we 
-# want to initialize the structure
-def initialize(connections):
-	N = connections.shape[0]
-	# Here we have one-dimension
-	placement = {}
-	for i in xrange(N):
-		x = np.random.randint(1,100)
-		
+alpha = 0.99
+threshold = 0.01
 
-# Suppose you are any state say S, then there can be say k possible neighbourhoods
-# In that case, call this function k times. This set of neighbourhood will form set of S'
-def findNeighbour(positions):
-	# given a placement find new placement
-	# In one-dim, we may simply flip x-coordinates
-	node1 = np.random.randint(1,10)
-	node2 = np.random.randint(1,10)
-	# Now flip the position of these nodes
-	temp = position[str(node1)]
-	position[str(node1)] = position[str(node2)]
-	position[str(node2)] = temp
-	
-	# simialrly we can flip some m positions, if required
-	return positions
+# we want to initialize the structure
+# i.e. place the k-elements on the N X N grid
+def initialize(k, N, grid):
+
+	for element in xrange(1,k+1):
+		assigned = False
+		while(not assigned):
+			i = np.random.randint(N)
+			j = np.random.randint(N)
+			if(grid[i,j]==0):
+				grid[i,j] = element
+				assigned = True
+	return grid
+
+# Suppose you are any state say S, then you can go to 
+# some other state S'
+def findNeighbourState(N, k, grid):
+	# We will follow these two protocols, each 50% of time
+	# 1. swap positions of any two elements
+	# 2. Move any element to a new position in the grid
+
+	tempGrid = np.array(grid)
+	if(np.random.random()<0.5):
+		element1 = np.random.randint(1, k+1)
+		element2 = np.random.randint(1, k+1)
+		while(element2==element1):
+			element2 = np.random.randint(1, k+1)
+		(x1,y1) = zip(*np.where(tempGrid == element1))[0]
+		(x2,y2) = zip(*np.where(tempGrid == element2))[0]
+		tempGrid[x1, y1] = element2
+		tempGrid[x2, y2] = element1
+	else:
+		element1 = np.random.randint(1, k+1)
+		(x1,y1) = zip(*np.where(tempGrid == element1))[0]
+		# find an empty place
+		temp = zip(*np.where(tempGrid == 0))
+		(x2,y2) = temp[np.random.randint(len(temp))]
+		tempGrid[x1, y1] = 0
+		tempGrid[x2, y2] = element1		
+
+	return tempGrid
+
 
 # This will return the updated temperature 
-# according to some schedule
+# according to some annealing schedule
 def updateTemp(T):
 	return alpha*T
 
-def annealing(positions,T):
+
+# This function finds the total wirelength
+def cost(k, grid, connections):
+	distance = 0
+	for element in xrange(1,k+1):
+		(x1,y1) = zip(*np.where(grid == element))[0]
+		for nextElement in range(element+1,k+1):
+			if(connections[element,nextElement]):
+				(x2,y2) = zip(*np.where(grid == nextElement))[0]
+				distance += np.absolute(x2-x1) + np.absolute(y2-y1)
+	return distance
+
+
+def annealing(N, k, grid, connections):
+	T = INF
+	# We initialize the grid
+	grid = initialize(k, N, grid)
+	minCost = cost(k, grid, connections)
+
+	print "Initial Cost",minCost
+	print "Initial Grid"
+	print grid	
+
 	# No. of interation at each temperature
 	# No. of temperature points to try
-    while (T > threshold):        
-		nextOrder = nextArrangement(currentOrder);
-            deltaDistance = GetTotalDistance(nextOrder) - distance;
-                if ():
-                    for i in range(len(nextOrder):
-                        currentOrder[i] = nextOrder[i];
+	while(T > threshold):
+		tempGrid = findNeighbourState(N, k, grid)
+		tempCost = cost(k, tempGrid, connections)
+		delta = tempCost - minCost
+		if (delta<0):
+			grid = tempGrid
+			minCost = tempCost
+		else:
+			p = np.exp(-delta / T)
+			if(np.random.random()<p):
+				grid = tempGrid
+				minCost = tempCost
+		T = updateTemp(T)
 
-                    distance = deltaDistance + distance;
-                temperature *= coolingRate;
-
-                iteration++;
-
-# This function finds the cost of any state S'
-def cost():
-
+	return grid,minCost
 
 def main():
-	# Lets say size of each block is one unit
-	# i.e. if a,b,c are connected serially, the min wirelength would be 2
-	# Initially we only know who is connected with whome
-	# We will use 2-d numpy array
-	# one will represent there is connection between i and j, 0 otherwise
-	connections = np.empty([3,3])
-	connections[0,1] = 1
-	#---------Input done---------
 
-	init = initialize(connections)
-	
-	T = INF
-	
+	# For simplicty, assume that the element to be paced is point sized.
+	# And we have to place these k-point sized element on N X N grid.
 
-	
+	N = 5
+	k = 5
+
+	# We will store the positions of the elements in the N X N grid
+	grid = np.zeros([N,N])
+	connections = np.zeros([k+1,k+1])
+
+	# Assuming that each element can have two input and one output.
+	# Initially we only know who is connected with whome. We use 2-d
+	# array 1 will represent there is connection between i and j,
+	# 0 otherwise
+	#filling the values in only upper triangular matrix
+	connections[1,3] = 1
+	connections[2,3] = 1
+	connections[4,5] = 1
+	connections[3,5] = 1
+
+	# connections[3,1] = 1
+	# connections[3,2] = 1
+
+	finalGrid,cost = annealing(N, k, grid, connections)
+	print "Final Cost", cost
+	print "Final Grid"
+	print finalGrid
+
+main()
